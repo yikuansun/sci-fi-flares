79461f6dfec7065febf91ef75df7f0156f9cbfab
yikuansun <38194363+yikuansun@users.noreply.github.com>
2020-09-02 15:00:15 -0400

    uploaded files
    heck yeah

---
+++ code.js
@@ -0,0 +1,26 @@
+//initiate vars
+if (location.search == "") {
+    location.replace("index.html");
+}
+else {
+    docWidth = parseFloat(location.search.split("canvaswidth=")[1]);
+    docHeight = parseFloat(location.search.split("canvasheight=")[1]);
+}
+
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext("2d");
+ctx.globalCompositeOperation = 'screen'; //due to absense of linear dodge (add), which is preferable imo
+
+function addArtifact(imgsrc, centerx, centery, width, height) {
+    image = new Image();
+    image.src = imgsrc;
+    x = centerx - width/2;
+    y = centery - height/2;
+    ctx.drawImage(image, x, y, width, height);
+}
+
+function draw() {
+
+    //update preview
+    document.getElementById("flarepreview").setAttribute("src", canvas.toDataURL("image/jpg"));
+}
