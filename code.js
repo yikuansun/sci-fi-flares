eb61bdf8af2ac2a3c741e974b44c148c54f119bb
Yikuan Sun <yikuansun2015@gmail.com>
2020-09-03 16:02:29 -0400

    changed stuff ig. does not work at all yet.
    sad :cry: :-1:

--- code.js
+++ code.js
@@ -8,6 +8,8 @@ if (Number.isNaN(docWidth) || Number.isNaN(docHeight)) {
 }
 
 canvas = document.getElementById('canvas');
+canvas.width = docWidth;
+canvas.height = docHeight;
 ctx = canvas.getContext("2d");
 ctx.globalCompositeOperation = 'screen'; //due to absense of linear dodge (add), which is preferable imo
 
@@ -16,11 +18,18 @@ function addArtifact(imgsrc, centerx, centery, width, height) {
     image.src = imgsrc;
     x = centerx - width/2;
     y = centery - height/2;
+    ctx = canvas.getContext("2d");
     ctx.drawImage(image, x, y, width, height);
 }
 
 function draw() {
 
+    //add elements
+    addArtifact("iris1.png", docWidth / 2, docHeight / 2, 20, 20);
+
     //update preview
     document.getElementById("flarepreview").setAttribute("src", canvas.toDataURL("image/jpg"));
 }
+
+document.getElementsByTagName("button")[0].onclick = draw;
+draw();
