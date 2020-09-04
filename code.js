74b4788b0e8cc3fd0531c7113bf95b8d817a71f9
Yikuan Sun <yikuansun2015@gmail.com>
2020-09-04 14:46:33 -0400

    Update code.js

--- code.js
+++ code.js
@@ -35,8 +35,8 @@ function draw() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     //add elements
-    addArtifact("streakleft1.png", flarecenter[0], flarecenter[1], docHeight - (flarecenter[0] - docWidth / 2), docHeight / 7);
-    addArtifact("streakright1.png", flarecenter[0], flarecenter[1], docHeight + (flarecenter[0] - docWidth / 2), docHeight / 7);
+    addArtifact("streakleft1.png", flarecenter[0], flarecenter[1], docHeight + (flarecenter[0] - docWidth / 2), docHeight / 7);
+    addArtifact("streakright1.png", flarecenter[0], flarecenter[1], docHeight - (flarecenter[0] - docWidth / 2), docHeight / 7);
     addArtifact("hotspot1.png", flarecenter[0], flarecenter[1], docHeight / 6, docHeight / 6);
     addArtifact("iris1.png", docWidth / 2, docHeight / 2, docHeight / 10, docHeight / 10);
 
