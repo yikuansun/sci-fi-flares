cefeab0626c912326a566d846c845cdd89d25973
Yikuan Sun <yikuansun2015@gmail.com>
2020-11-04 18:57:49 -0500

    fixed preload

--- preload_imgs.js
+++ preload_imgs.js
@@ -3,6 +3,16 @@ function preload() {
     for (var i = 0; i < arguments.length; i++) {
         images[i] = new Image();
         images[i].src = preload.arguments[i];
+        document.body.appendChild(images[i]);
+        images[i].onload = function() { this.remove(); };
+        if (i == arguments.length - 1) {
+            images[i].onload = function() {
+                this.remove();
+                mainscript = document.createElement("script");
+                mainscript.src = "code.js";
+                document.body.appendChild(mainscript);
+            };
+        }
     }
 }
 
