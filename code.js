ada546fd4c3bd68c9250bde9983c208926cdc7f5
Yikuan Sun <yikuansun2015@gmail.com>
2020-12-28 19:59:46 -0500

    Update code.js

--- code.js
+++ code.js
@@ -63,10 +63,10 @@ function draw() {
         addArtifact("textures/streakright" + streaktype + ".png", flarecenter[0], flarecenter[1], (docWidth - (flarecenter[0] - docWidth / 2) * 1.5) / 1.25 * streakscale[0], docHeight / 5 * streakscale[1]);
     }
     if (hotspottype != "None") {
-        addArtifact("textures/hotspot" + hotspottype + ".png", flarecenter[0], flarecenter[1], docHeight / 4 * hotspotscale[0], docHeight / 4 * hotspotscale[1]);
+        addArtifact("textures/hotspot" + hotspottype + ".png", flarecenter[0], flarecenter[1], docHeight / 2.7 * hotspotscale[0], docHeight / 2.7 * hotspotscale[1]);
     }
     if (halotype != "None") {
-        addArtifact("textures/halo" + halotype + ".png", flarecenter[0], flarecenter[1], docHeight / 1.75 * haloscale[0], docHeight / 1.75 * haloscale[1]);
+        addArtifact("textures/halo" + halotype + ".png", flarecenter[0], flarecenter[1], docHeight / 1.2 * haloscale[0], docHeight / 1.2 * haloscale[1]);
     }
     if (iristype != "None") {
         Math.seedrandom(NaN);
